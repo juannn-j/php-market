0c62bd22bbb290dbd770b2ebeced3450b7bc43b8
juannn-j <lainfhj@proton.me>
2025-06-17 16:24:19 -0500

    add pags with css and bootstrap, adn sessions variables

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div>
     <?php
-        include '../presentacion/templates/navbar';
+        include './navbar.php';
     ?>
 </div>   
 </body>
