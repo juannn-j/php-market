3eb7391643641541e51b2c12f3d122dc4aa60cb5
ElKratos259 <elkratosxdd@outlook.com>
2025-06-23 16:45:35 -0500

    Actualizacion de query

--- script.sql
+++ script.sql
@@ -38,10 +38,10 @@ CREATE TABLE pedido_detalles (
 
 INSERT INTO usuarios (nombre, email, password, tipo)
 VALUES 
-('Juan Pérez', 'juan@example.com', '1234', 'C'),  -- C = Cliente
-('Admin General', 'admin@example.com', 'admin123', 'A');  -- A = Administrador
+('Juan Pérez', 'juan@example.com', '1234', 'C'), -- C = Cliente
+('Admin General', 'admin@example.com', 'admin123', 'A'); -- A = Administrador
 
 INSERT INTO articulos (nombre, marca, descripcion, precio, stock, imagen_url)
 VALUES 
-('Lenovo IdeaPad 3', 'Lenovo', 'Laptop básica con procesador AMD Ryzen 5, 8GB RAM, SSD 256GB.', 1899.99, 10, 'https://example.com/lenovo.jpg'),
-('HP Pavilion x360', 'HP', 'Laptop convertible 2 en 1 con pantalla táctil, 16GB RAM, Intel i5.', 2799.00, 5, 'https://example.com/hp.jpg');
+('Lenovo IdeaPad 3', 'Lenovo', 'Laptop básica con procesador AMD Ryzen 5, 8GB RAM, SSD 256GB.', 1899.99, 10, '"https://p1-ofp.static.pub/medias/bWFzdGVyfHJvb3R8MjY5MjEzfGltYWdlL3BuZ3xoODYvaDUzLzE0MTg2OTE5NTkxOTY2LnBuZ3w2ODgwOTdhZDhlODAwNTYzZmVlNDcwNzE5MGI3MzEzMWNiMTIxYmY5NWE3MzcxZDA1NzM2MzkwNWRlYzQ0MDU3/lenovo-laptop-ideapad-3-14-intel-subseries-hero.png"'),
+('HP Pavilion x360', 'HP', 'Laptop convertible 2 en 1 con pantalla táctil, 16GB RAM, Intel i5.', 2799.00, 5, '"https://media.falabella.com/falabellaPE/142585094_01/w=1500,h=1500,fit=pad"');
